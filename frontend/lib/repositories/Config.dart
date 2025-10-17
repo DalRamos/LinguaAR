6bf022b2562924d177fb049a66f32bdb26c21bca
ZhyrexCalamiong <xeryhzcalamiong@gmail.com>
2025-10-17 20:14:14 +0800

    added api for fsls

--- frontend/lib/repositories/Config.dart
+++ frontend/lib/repositories/Config.dart
@@ -6,3 +6,7 @@ class Cloud_url {
 class BasicUrl {
   static const String baseURL = "https://linguar-deploy.onrender.com";
 }
+
+class Fsl {
+  static const String fslUrl = "http://192.168.100.66:5000";
+}
